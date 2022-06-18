5d20854a42d1834503ac127902833a4d292f6c18
mohamedhany7 <mohameddhanyy7@gmail.com>
2022-06-18 18:47:03 +0200

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Mohamed Hany on 18/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
