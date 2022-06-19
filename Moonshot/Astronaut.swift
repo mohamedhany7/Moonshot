d8273d84f5321872e1ac8c30d791f264666cd862
mohamedhany7 <mohameddhanyy7@gmail.com>
2022-06-20 00:07:56 +0200

    Second Commit

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Mohamed Hany on 18/06/2022.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
